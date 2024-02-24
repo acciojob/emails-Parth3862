21a424ff73fc7bbd4e6c3443f45dc5cb44b3d3f3
Parth <parthudgirkar@gmail.com>
2024-02-24 12:23:22 +0530

    first commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Email {
 
-    private String emailId;
+    private final String emailId;
     private String password;
 
     public Email(String emailId){
@@ -11,19 +11,46 @@ public class Email {
     }
 
     public String getEmailId() {
+
         return emailId;
     }
 
     public String getPassword() {
+
         return password;
     }
 
     public void changePassword(String oldPassword, String newPassword){
-        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        //Change password only if the oldPassword is equal to current password
+        // and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if (oldPassword.equals(password)) {
+            boolean hasUppercase = false;
+            boolean hasLowercase = false;
+            boolean hasDigit = false;
+            boolean hasSpecialChar = false;
+
+            if (newPassword.length() >= 8) {
+                for (char ch : newPassword.toCharArray()) {
+                    if (Character.isUpperCase(ch)) {
+                        hasUppercase = true;
+                    } else if (Character.isLowerCase(ch)) {
+                        hasLowercase = true;
+                    } else if (Character.isDigit(ch)) {
+                        hasDigit = true;
+                    } else {
+                        hasSpecialChar = true;
+                    }
+                }
+                if (hasUppercase && hasLowercase && hasDigit && hasSpecialChar) {
+                    password = newPassword;
+                }
+            }
+        }
     }
 }
--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -5,11 +5,19 @@ import java.util.Date;
 
 public class Gmail extends Email {
 
-    int inboxCapacity; //maximum number of mails inbox can store
-    //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String). It is guaranteed that message is distinct for all mails.
+    int inboxCapacity;
+    private  ArrayList<Mail> inbox;
+    private  ArrayList<Mail> trash;
+
+    //maximum number of mails inbox can store
+    //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String).
+    // It is guaranteed that message is distinct for all mails.
     //Trash: Stores mails. Each mail has date (Date), sender (String), message (String)
     public Gmail(String emailId, int inboxCapacity) {
-
+        super(emailId);
+        this.inboxCapacity = inboxCapacity;
+        this.inbox = new ArrayList<>();
+        this.trash = new ArrayList<>();
     }
 
     public void receiveMail(Date date, String sender, String message){
@@ -17,49 +25,81 @@ public class Gmail extends Email {
         // It is guaranteed that:
         // 1. Each mail in the inbox is distinct.
         // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
-
+        if(inbox.size()>=inboxCapacity){
+            moveOldestMailToTrash();
+        }
+        inbox.add(new Mail(date,sender,message));
+    }
+    public void moveOldestMailToTrash(){
+        Mail oldestMail = inbox.get(0);
+        for (Mail mail:inbox){
+            if(mail.getDate().before(oldestMail.getDate())){
+                oldestMail=mail;
+            }
+        }
+        inbox.remove(oldestMail);
+        trash.add(oldestMail);
     }
 
     public void deleteMail(String message){
         // Each message is distinct
         // If the given message is found in any mail in the inbox, move the mail to trash, else do nothing
-
+        for(Mail mail:inbox){
+            if(mail.getMessage().equals(message)){
+                trash.add(mail);
+                inbox.remove(mail);
+                break;
+            }
+        }
     }
 
-    public String findLatestMessage(){
+    public String findLatestMessage() {
         // If the inbox is empty, return null
-        // Else, return the message of the latest mail present in the inbox
-
+        if (!inbox.isEmpty()) {
+            // Return the message of the last (latest) mail present in the inbox
+            return inbox.get(inbox.size() - 1).getMessage();
+        } else {
+            return null;
+        }
     }
 
-    public String findOldestMessage(){
-        // If the inbox is empty, return null
-        // Else, return the message of the oldest mail present in the inbox
-
+    public String findOldestMessage() {
+        if (!inbox.isEmpty()) {
+            return inbox.get(0).getMessage();
+        } else {
+            return null;
+        }
     }
-
     public int findMailsBetweenDates(Date start, Date end){
         //find number of mails in the inbox which are received between given dates
+        int count=0;
+        for(Mail mail:inbox){
+            if (mail.getDate().compareTo(start)>=0 && mail.getDate().compareTo(end)<=0) {
+                count++;
+            }
+        }
+        return count;
         //It is guaranteed that start date <= end date
 
     }
 
     public int getInboxSize(){
         // Return number of mails in inbox
-
+        return inbox.size();
     }
 
     public int getTrashSize(){
         // Return number of mails in Trash
-
+        return trash.size();
     }
 
     public void emptyTrash(){
         // clear all mails in the trash
-
+        trash.clear();
     }
 
     public int getInboxCapacity() {
         // Return the maximum number of mails that can be stored in the inbox
+        return inboxCapacity;
     }
 }
